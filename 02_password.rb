2ccb5983c8dff8b5bc92f64521e92b5da8a0170d
typhain <typhaine.charrat@gmail.com>
2019-07-11 17:32:29 +0200

    password

--- 02_password.rb
+++ 02_password.rb
@@ -1,18 +1,29 @@
 def sign_up
-puts "retapez votre mot de passe"
+puts "Définissez un mot de passe"
 sign_up = gets.chomp
 return sign_up
 end
 
-def login
+def login(password)
   puts "Veuillez entrer votre mot de passe"
   print "> "
   login = gets.chomp
-    while sign_up != login
+    while password != login
       puts "Mot de passe incorrect. Réessayer"
       print "> "
       login = gets.chomp
     end
 end
 
-puts login
+def welcome_screen
+  puts "Bienvenue dans votre espace secret. "
+  puts "Voici les plans de l'application de Lucas Hudson"
+end
+
+def perform
+password = sign_up
+login(password)
+welcome_screen
+end
+
+perform
