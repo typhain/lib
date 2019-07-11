131326a83aa925786f40744ace84f2e2575134b9
typhain <typhaine.charrat@gmail.com>
2019-07-11 10:22:30 +0200

    combo3

---
+++ 00_hello.rb
@@ -0,0 +1,16 @@
+def ask_first_name
+  puts"quel est ton prénom?"
+  first_name = gets.chomp
+  first_name
+end
+
+def say_hello(first_name, prefix = "Bonjour")
+  puts "#{prefix} #{first_name}"
+end
+
+def perform
+  first_name = ask_first_name
+  say_hello (first_name)
+end
+
+perform
