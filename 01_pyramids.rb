5cccf6e11872fb6b8d9850292c2184814207cbd8
thibobo78 <thibault.collard@gmail.com>
2019-07-11 11:01:24 +0200

    Pyramide entière

---
+++ 01_pyramids.rb
@@ -0,0 +1,23 @@
+def half_pyramid
+
+puts "Bienvenue dans les marches de l'enfer, jusqu'a combien d'etages souhaites-tu aller ?"
+print " >"
+step_hell = gets.chomp.to_i
+
+step_hell.times do |i|
+
+print " "*(step_hell - 1)
+print "#"*(i+1)
+print "#"*(i)
+print "\n"
+
+step_hell = step_hell - 1
+
+end
+end
+
+def perform
+puts half_pyramid
+end
+
+perform
